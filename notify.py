4035d7074dde9ac86c6e744ff4b830d411ff340a
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-07-31 23:54:26 +0530

    Project Completed with necessary changes

--- notify.py
+++ notify.py
@@ -20,8 +20,34 @@ if __name__ == '__main__':
     my_HTML_data = getData("https://www.mygov.in/corona-data/covid19-statewise-status")
     soup = BeautifulSoup(my_HTML_data,'html.parser')
     # print(soup.prettify())
-    myData = ""
-    for item in soup.select("div.field-collection-item-field-covid-statewise-data"):
-        print(item.get_text())
+    divLookup = ["div.field-name-field-select-state","div.field-name-field-total-confirmed-indians","div.field-name-field-cured","div.field-name-field-deaths"]
+    StateName = []
+    Confirmed = []
+    Cured = []
+    Deaths = []
+    for item in soup.select(divLookup[0]):
+        StateName.append(item.get_text().replace(u'\xa0', u' '))
 
-    # print(myData)
+    for item in soup.select(divLookup[1]):
+        # pass
+        Confirmed.append(item.get_text().replace(u'\xa0', u' '))
+
+
+    for item in soup.select(divLookup[2]):
+        # pass
+        Cured.append(item.get_text().replace(u'\xa0', u' '))
+
+
+    for item in soup.select(divLookup[3]):
+        # pass
+        Deaths.append(item.get_text().replace(u'\xa0', u' '))
+
+    x = zip(StateName,Confirmed,Cured,Deaths)
+    x = list(x)
+    for stat in x:
+        stateName,totalConfirmed,totalCured,totalDeaths = stat
+        if stateName in ['State Name: Odisha','State Name: Andhra Pradesh'] :  # Can add other states
+            print(stat)
+            nTitle = "COVID-19 Info"
+            nText = f"{stateName}  {totalConfirmed}  {totalCured}  {totalDeaths}"
+            notifyMe(nTitle, nText)
